5b5bf006d94ba54b68253dab673926b88a956b92
Dmitrii <you@example.com>
2022-04-13 14:13:29 +0700

    js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
    const modalWindow = document.querySelector('.modal');
    const buttonsModal = document.querySelectorAll('.button-play');
+   const frame = document.querySelector('.frame');
+   const videoUrl = "https://www.youtube.com/embed/320TyBx43j8";
 
    buttonsModal.forEach((item, i) => {
       item.addEventListener('click', () => {
          modalWindow.classList.add('active');
+         frame.setAttribute('src', videoUrl);
       });
    });
 
    modalWindow.addEventListener('click', () => {
       modalWindow.classList.remove('active');
+      frame.setAttribute('src', '404');
    });
 });
